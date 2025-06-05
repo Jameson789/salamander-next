60e65cccd4532fd7d61626ca0cac8b64d297abaf
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-06-05 11:26:49 -0700

    Added startProcess component

--- src/app/preview/[filename]/page.jsx
+++ src/app/preview/[filename]/page.jsx
@@ -2,12 +2,14 @@
 
 import NavBar from "@/components/navBar"
 import PreviewVideo from "@/components/PreviewVideo"
+import StartProcess from "@/components/StartProcess"
 
 export default function VideoChooserPage() {
     return (
       <>
         <NavBar />
         <PreviewVideo />
+        <StartProcess />
       </>
     )
 }
