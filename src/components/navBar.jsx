f666b12a8fbe8b39c56c34b9f7d99a2e2e6a6ffa
dannymccarragher <dannymccarragher@gmail.com>
2025-06-10 11:30:26 -0700

    MUI styling for NavBar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,13 +1,60 @@
 import Link from "next/link";
+import { 
+  AppBar, 
+  Toolbar, 
+  Typography, 
+  Button, 
+  Box,
+  Container 
+} from "@mui/material";
+import MovieIcon from '@mui/icons-material/Movie';
 
 const NavBar = () => {
-    return (
-        <div>
-            <nav>
-                <Link href="/videos"> All Videos </Link>
-            </nav>
-        </div>
-    )
-}
+  return (
+    <AppBar position="static" sx={{ bgcolor: 'white', boxShadow: 1 }}>
+      <Container maxWidth="lg">
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          {/* Logo/Brand */}
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <MovieIcon sx={{ color: 'primary.main', mr: 1 }} />
+            <Typography 
+              variant="h6" 
+              component="div"
+              sx={{ color: 'primary.main', fontWeight: 'bold' }}
+            >
+              Salamander
+            </Typography>
+          </Box>
+
+          {/* Navigation Links */}
+          <Box>
+            <Button 
+              component={Link} 
+              href="/"
+              sx={{ 
+                color: 'text.primary',
+                mx: 1,
+                '&:hover': { bgcolor: 'grey.100' }
+              }}
+            >
+              Home
+            </Button>
+            <Button 
+              component={Link} 
+              href="/videos"
+              sx={{ 
+                color: 'text.primary',
+                mx: 1,
+                '&:hover': { bgcolor: 'grey.100' }
+              }}
+            >
+              Videos
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default NavBar;
